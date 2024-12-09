89eb6c3d64afac3f30b612081fc07bde11aec811
RAFA12763 <143661914+RAFA12763@users.noreply.github.com>
2024-12-09 11:34:05 -0300

    Update ANTIVOID_XUNAXI_SCRIPTS_SLAP_BATTLES.lua

--- ANTIVOID_XUNAXI_SCRIPTS_SLAP_BATTLES.lua
+++ ANTIVOID_XUNAXI_SCRIPTS_SLAP_BATTLES.lua
@@ -3,7 +3,3 @@ game.Workspace.dedBarrier.Position =  Vector3.new(15, -17, 41.5)
 
 game.Workspace.dedBarrier.CanCollide = true
 game.Workspace.TAntiVoid.CanCollide = true
-
-print("ANTI VOID ATIVADO")
-
-end 
