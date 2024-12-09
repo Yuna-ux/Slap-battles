3d0c4e84bdb27bdf20cff6a357304d9216595727
RAFA12763 <143661914+RAFA12763@users.noreply.github.com>
2024-12-09 13:14:54 -0300

    Update Slap_aura_script_completo.lua

--- Slap_aura_script_completo.lua
+++ Slap_aura_script_completo.lua
@@ -1,21 +1,9 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RAFA12763/Scripts/refs/heads/main/SLAP_AURA_MADE_BY_XUNAXI_SCRIPTS.lua"))();
+-- Carregando os scripts externos com loadstring
+local slapAuraScript = game:HttpGet("https://raw.githubusercontent.com/RAFA12763/Scripts/refs/heads/main/SLAP_AURA_MADE_BY_XUNAXI_SCRIPTS.lua")
+local buttonsScript = game:HttpGet("https://raw.githubusercontent.com/RAFA12763/Scripts/refs/heads/main/BOTÕES_SLAP_AURA_XUNAXI.lua")
+local antiVoidScript = game:HttpGet("https://raw.githubusercontent.com/RAFA12763/Scripts/refs/heads/main/ANTIVOID_XUNAXI_SCRIPTS_SLAP_BATTLES.lua")
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RAFA12763/Scripts/refs/heads/main/BOTÕES_SLAP_AURA_XUNAXI.lua"))();
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RAFA12763/Scripts/refs/heads/main/ANTIVOID_XUNAXI_SCRIPTS_SLAP_BATTLES.lua"))();
-
-if hookmetamethod then
-local bypass;
-    bypass = hookmetamethod(game, "__namecall", function(method, ...) 
-        if getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.Ban then
-            return
-        elseif getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.AdminGUI then
-            return
-        elseif getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.WalkSpeedChanged then
-            return
-        end
-        return bypass(method, ...)
-    end)
- else
-OrionLib:MakeNotification({Name = "Error",Content = "You need to be careful, You are using executor [ "..identifyexecutor().." ] There is no bypass",Image = "rbxassetid://7733658504",Time = 5})
-end
+-- Executando os scripts
+loadstring(slapAuraScript)()
+loadstring(buttonsScript)()
+loadstring(antiVoidScript)()
