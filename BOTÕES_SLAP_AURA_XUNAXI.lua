d8f54cbb42b6485f5005de1ee30fcbe709ec4e88
RAFA12763 <143661914+RAFA12763@users.noreply.github.com>
2024-12-08 21:49:46 -0300

    Update BOTÕES_SLAP_AURA_XUNAXI.lua

--- BOTÕES_SLAP_AURA_XUNAXI.lua
+++ BOTÕES_SLAP_AURA_XUNAXI.lua
@@ -1,6 +1,19 @@
+
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
+
+-- Função para ajustar a velocidade constantemente
+local function setLoopSpeed()
+    while true do
+        humanoid.WalkSpeed = 40 -- Define a velocidade para 40
+        wait(0.1) -- Aguarda 0.1 segundos antes de verificar novamente
+    end
+end
+
+-- Executa a função em uma nova thread para manter o loop ativo
+spawn(setLoopSpeed)
+
 
 -- Criando o ScreenGui
 local screenGui = Instance.new("ScreenGui")
