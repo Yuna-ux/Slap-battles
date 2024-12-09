85a82380066d9638ae9bf3b5ff423b37c550bd24
RAFA12763 <143661914+RAFA12763@users.noreply.github.com>
2024-12-09 11:19:39 -0300

    Update BOTÕES_SLAP_AURA_XUNAXI.lua

--- BOTÕES_SLAP_AURA_XUNAXI.lua
+++ BOTÕES_SLAP_AURA_XUNAXI.lua
@@ -169,9 +169,3 @@ game:GetService("RunService").RenderStepped:Connect(function()
         slapClosestPlayer2()
     end
 end)
-
-game.Workspace.dedBarrier.Position =  Vector3.new(15, -17, 41.5)
-
-game.Workspace.dedBarrier.CanCollide = true
-game.Workspace.TAntiVoid.CanCollide = true
-
