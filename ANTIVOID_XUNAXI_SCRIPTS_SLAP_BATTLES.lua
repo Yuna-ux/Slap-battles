aaa14e0260e31aa0ae2673836b1af84b556fcf34
RAFA12763 <143661914+RAFA12763@users.noreply.github.com>
2024-12-11 00:00:23 -0300

    Update ANTIVOID_XUNAXI_SCRIPTS_SLAP_BATTLES.lua

--- ANTIVOID_XUNAXI_SCRIPTS_SLAP_BATTLES.lua
+++ ANTIVOID_XUNAXI_SCRIPTS_SLAP_BATTLES.lua
@@ -1,8 +1,10 @@
---ANTIVOID XUNAXI_SCRIPTS
-local antivoid = game.Workspace:FindFirstChild("antivoid")
-if antivoid then
-    antivoid.Position = Vector3.new(15, -17, 41.5)
-    antivoid.CanCollide = true
+-- DED BARRIER XUNAXI_SCRIPTS
+local dedBarrier = game.Workspace:FindFirstChild("dedBarrier")
+
+if dedBarrier then
+    -- Modificar a posição e as propriedades do objeto
+    dedBarrier.Position = Vector3.new(15, -17, 41.5)
+    dedBarrier.CanCollide = true
 else
-    warn("Barreira não encontrado no Workspace!")
+    warn("Barreira 'dedBarrier' não encontrada no Workspace!")
 end
