e057b3b00ccfb8691e6b815c7c581012fb9922a2
RAFA12763 <143661914+RAFA12763@users.noreply.github.com>
2024-12-14 11:51:11 -0300

    Delete Slap_aura_script_completo.lua

--- Slap_aura_script_completo.lua
+++
@@ -1,9 +0,0 @@
--- Carregando os scripts externos com loadstring
-local slapAuraScript = game:HttpGet("https://raw.githubusercontent.com/RAFA12763/Scripts/refs/heads/main/SLAP_AURA_MADE_BY_XUNAXI_SCRIPTS.lua")
-local buttonsScript = game:HttpGet("https://raw.githubusercontent.com/RAFA12763/Scripts/refs/heads/main/BOTÕES_SLAP_AURA_XUNAXI.lua")
-local antiVoidScript = game:HttpGet("https://raw.githubusercontent.com/RAFA12763/Scripts/refs/heads/main/ANTIVOID_XUNAXI_SCRIPTS_SLAP_BATTLES.lua")
-
--- Executando os scripts
-loadstring(slapAuraScript)()
-loadstring(buttonsScript)()
-loadstring(antiVoidScript)()
