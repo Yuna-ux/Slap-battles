efc9f4bc3d49e63a15082ffc1414f8b523d25871
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-03-09 22:09:06 -0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,10 +1,14 @@
 -- Test Script
 local DefaultConfig = {
-test = true
+    test = 50
 }
 
 local function InitConfigs()
-   _G.test = _G.test or DefaultConfig.test
+    _G.test = _G.test or DefaultConfig.test
 end
 
 InitConfigs()
+
+if type(_G.test) == "number" then
+    print(_G.test)
+end
